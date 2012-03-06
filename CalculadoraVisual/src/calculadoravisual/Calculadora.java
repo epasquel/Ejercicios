47479a98d01dde4467ab818de83f8bf8b0cd2d53
epasquel <epasquel_20@hotmail.com>
2012-03-05 23:17:10 -0500

    Calculadora Visual

---
+++ CalculadoraVisual/src/calculadoravisual/Calculadora.java
@@ -0,0 +1,16 @@
+
+
+package calculadoravisual;
+
+
+public class Calculadora {
+
+    public double suma(double operando1, double operando2){
+        return operando1 + operando2;
+    }
+
+    public double resta(double operando1, double operando2){
+        return operando1 - operando2;
+    }
+
+}
