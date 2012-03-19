fbc32073a96d62af183923893927cd71dc949d46
Agustin Curu <acuruh@hotmail.com>
2012-03-18 23:05:09 -0500

    Actualizacion

--- ControlPrestamos/src/controladora/admPrestamosLibros.java
+++ ControlPrestamos/src/controladora/admPrestamosLibros.java
@@ -3,15 +3,22 @@ package controladora;
 
 import java.util.ArrayList;
 import modelos.cliente;
+import modelos.libro;
 
 public class admPrestamosLibros {
     
     private ArrayList<cliente> listClientes = new ArrayList<cliente>();
+    private ArrayList<libro> listLibros = new ArrayList<libro>();
     
     public void simularTabla(){
         listClientes.add(new cliente("C0001", "Ebert", "Pasquel", 1));
         listClientes.add(new cliente("C0002", "Joel", "Gamboa", 0));
         listClientes.add(new cliente("C0003", "Carlos", "Saldivar", 3));
+        listLibros.add(new libro("L0001", "POO para ingenieros", "material", true));
+        listLibros.add(new libro("L0002", "Etica y Ciudadania en el siglo XX", "material", true));
+        listLibros.add(new libro("L0003", "Manual de urbanidad y buenas costumbres", "material", false));
+        listLibros.add(new libro("L0004", "Ciencias naturales", "material", true));
+        listLibros.add(new libro("L0005", "Economia y estadistica 2012", "material", false));  
     }
     
     public cliente validarExistenciaCliente(String codCliente){
@@ -35,6 +42,19 @@ public class admPrestamosLibros {
             succes = true;
         }
         return succes;
+    }
+    
+    public boolean libroDisponible(String codLibro){
+        
+        simularTabla();        
+        libro objLibro = null;
+        for (libro objLib : listLibros){
+            if (objLib.getCodLibro().equals(codLibro) && objLib.getFlagEnable()==true){
+                return true;
+            }
+        }
+        return false;
+        
     }
     
     
