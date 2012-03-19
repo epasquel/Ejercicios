3e2495d2a5b94fc96f5009b908844b9d6db67851
Agustin Curu <acuruh@hotmail.com>
2012-03-18 23:05:20 -0500

    Actualizacion

--- ControlPrestamos/test/controlprestamos/ControlPrestamosTest.java
+++ ControlPrestamos/test/controlprestamos/ControlPrestamosTest.java
@@ -21,13 +21,24 @@ public class ControlPrestamosTest {
     @Test
     public void puedeSolicitarLibro(){
         assertTrue(objAdm.puedeSolicitarLibro("C0001"));
-        System.out.println("Puede solictar libro");
+        System.out.println("Puede solicitar libro");
     }
     
     @Test
     public void noPuedeSolicitarLibro(){
         assertFalse(objAdm.puedeSolicitarLibro("C0003"));
-        System.out.println("No puede solicitra libro");
+        System.out.println("No puede solicitar libro");
+    }
+    
+    @Test
+    public void libroNoDisponible(){
+        assertFalse(objAdm.libroDisponible("L0003"));
+        System.out.println("El libro no esta disponible");
+    }
+    @Test
+    public void libroDisponible(){
+        assertTrue(objAdm.libroDisponible("L0001"));
+        System.out.println("El libro esta disponible");
     }
     
 }
